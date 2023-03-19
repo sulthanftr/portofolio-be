fcb8281fe9b97c4e46ba47fdcc1164bc2d12818d
sulthanftr <sulthanftr@gmail.com>
2023-03-19 17:24:57 +0700

    add error handling for auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { FirebaseService } from 'src/firebase/firebase.service';
 import { UserEntity } from './entities/user.entity';
 import {
+    AuthError,
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
     UserCredential,
@@ -16,21 +17,42 @@ export class AuthService {
 
   public async login(email: string, password: string): Promise<Omit<UserEntity,'password'>> {
     try {
-        const userCredential = await signInWithEmailAndPassword(
-          this.firebaseService.auth,
-          email,
-          password,
+      const userCredential = await signInWithEmailAndPassword(
+        this.firebaseService.auth,
+        email,
+        password,
+      );
+
+      if (userCredential) {
+        const data = await this.prisma.user.findUnique({ where: { email } });
+        delete data.password;
+        return data;
+      };
+
+    } catch (error: unknown) {
+      const firebaseAuthError = error as AuthError;
+
+      console.log(`[FIREBASE AUTH ERROR CODE]: ${firebaseAuthError.code}`);
+  
+      if (firebaseAuthError.code === 'auth/wrong-password') {
+        throw new HttpException(
+          'Incorrect email or password.',
+          HttpStatus.FORBIDDEN,
         );
-
-        if (userCredential) {
-            const data = await this.prisma.user.findUnique({ where: { email } });
-            delete data.password;
-            return data;
-        };
-
-      } catch (error: unknown) {
-        console.warn(`[ERROR]: ${error}`);
       }
+      if (firebaseAuthError.code === 'auth/missing-email') {
+        throw new HttpException(
+          'Missing email.',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      else {
+        throw new HttpException(
+          `Internal server error (${firebaseAuthError.code}).`,
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
   }
 
   public async register(body: Omit<UserEntity, 'id'>): Promise<Omit<UserEntity,'password'>> {
@@ -49,7 +71,25 @@ export class AuthService {
       };
 
     } catch (error: unknown) {
-      console.warn(`[ERROR]: ${error}`);
+      const firebaseAuthError = error as AuthError;
+      
+      console.log(`[FIREBASE AUTH ERROR CODE]: ${firebaseAuthError.code}`);
+
+      if (firebaseAuthError.code === 'auth/email-already-in-use') {
+        throw new HttpException('Email already exists.', HttpStatus.CONFLICT);
+      }
+      if (firebaseAuthError.code === 'auth/missing-email') {
+        throw new HttpException(
+          'Missing email.',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      else {
+          throw new HttpException(
+          `Internal server error (${firebaseAuthError.code}).`,
+          HttpStatus.INTERNAL_SERVER_ERROR,
+          );
+      }
     }
   }
 }
